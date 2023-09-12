61ff537133c35a48f2cd9c205dfcaa385a4bad65
tegar <tegar.wahyu@ui.ac.id>
2023-09-12 23:27:07 +0700

    rename app

--- red_s_inventory/urls.py
+++ red_s_inventory/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for inventory_pokemon_tcg project.
+URL configuration for red_s_inventory project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
