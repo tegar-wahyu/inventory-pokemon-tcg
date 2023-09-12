e5bffaed10b4c3f584501c7c6f6940cd07b0ba48
tegar <tegar.wahyu@ui.ac.id>
2023-09-12 19:15:00 +0700

    match views to model

--- main/views.py
+++ main/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Tegar Wahyu Khisbulloh',
-        'class': 'PBP F'
+        'name': 'VStar Arceus',
+        'amount': 66,
+        'description': 'Legendary Pokemon',
     }
 
     return render(request, "main.html", context)
